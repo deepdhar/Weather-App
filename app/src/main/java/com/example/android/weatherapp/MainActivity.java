98df47956a9c607f50d9269c34dd06c7473289ee
deepdhar <dhar2017.slg@gmail.com>
2021-05-27 21:07:42 +0530

    updated app UI

--- app/src/main/java/com/example/android/weatherapp/MainActivity.java
+++ app/src/main/java/com/example/android/weatherapp/MainActivity.java
@@ -2,11 +2,18 @@ package com.example.android.weatherapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,10 +33,12 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView t1_temp, t2_city, t3_weather_type, t4_date, t5_feels_like, day_temp_textView, night_temp_textView;
+    private TextView t1_temp, t2_city, t3_weather_type, description_textView;
+    private TextView wind_speed_textView, humidity_textView, visibility_textView, search_city_bn;
     private Button citySearchButton;
     private EditText cityEditText;
     private ImageView weatherIcon;
+    private LinearLayout homeLinearLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,16 +48,17 @@ public class MainActivity extends AppCompatActivity {
         t1_temp = (TextView) findViewById(R.id.temp_textView);
         t2_city = (TextView) findViewById(R.id.city_textView);
         t3_weather_type = (TextView) findViewById(R.id.weather_type_textView);
-        t4_date = (TextView) findViewById(R.id.date_textView);
-        t5_feels_like = (TextView) findViewById(R.id.feelsLike_textView);
-        day_temp_textView = (TextView) findViewById(R.id.day_temp_textView);
-        night_temp_textView = (TextView) findViewById(R.id.night_temp_textView);
-        citySearchButton = (Button) findViewById(R.id.citySearch_button);
+        search_city_bn = (TextView) findViewById(R.id.citySearch_button);
         cityEditText = (EditText) findViewById(R.id.city_editText);
         weatherIcon = (ImageView) findViewById(R.id.weather_icon);
+        wind_speed_textView = (TextView) findViewById(R.id.wind_speed_tv);
+        humidity_textView = (TextView) findViewById(R.id.humidity_tv);
+        visibility_textView = (TextView) findViewById(R.id.visibility_tv);
+        description_textView = (TextView) findViewById(R.id.desc_tv);
 
+        homeLinearLayout = (LinearLayout) findViewById(R.id.home_linearLayout);
 
-        citySearchButton.setOnClickListener(new View.OnClickListener() {
+        search_city_bn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 findWeather();
@@ -57,7 +67,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void findWeather() {
-        String apiKey = YOUR_API_KEY;
+        String apiKey = "4ebddb9e5d7ab62c4abd38ed54c920bc";
         String city = cityEditText.getText().toString();
         String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey;
 
@@ -71,50 +81,69 @@ public class MainActivity extends AppCompatActivity {
                     String temp = main_object.getString("temp");
                     String feelsLike = main_object.getString("feels_like");
                     String weatherType = object.getString("main");
+                    String desc = object.getString("description");
                     String cityName = response.getString("name");
                     String day = main_object.getString("temp_max");
                     String night = main_object.getString("temp_min");
 
-                    double max = Double.parseDouble(day) - 273.15;
-                    int maxC = (int) Math.round(max);
-                    day_temp_textView.setText(maxC + "°↑");
-
-                    double min = Double.parseDouble(night) - 273.15;
-                    int minC = (int) Math.round(min);
-                    night_temp_textView.setText(minC + "°↓");
-
-                    if(weatherType.equals("Thunderstorm"))
+                    if(weatherType.equals("Thunderstorm")) {
                         weatherIcon.setImageResource(R.drawable.thunderstorm);
-                    else if(weatherType.equals("Drizzle"))
+                        homeLinearLayout.setBackgroundResource(R.drawable.rainy_bg);
+                    }
+                    else if(weatherType.equals("Drizzle") || weatherType.equals("Rain")) {
                         weatherIcon.setImageResource(R.drawable.showers);
-                    else if(weatherType.equals("Snow"))
+                        homeLinearLayout.setBackgroundResource(R.drawable.rainy_bg);
+                    }
+                    else if(weatherType.equals("Snow")) {
                         weatherIcon.setImageResource(R.drawable.snow);
+                        homeLinearLayout.setBackgroundResource(R.drawable.cloudy_bg);
+                    }
                     else if(weatherType.equals("Mist") || weatherType.equals("Smoke") || weatherType.equals("Haze") || weatherType.equals("Dust")
                     || weatherType.equals("Fog") || weatherType.equals("Sand") || weatherType.equals("Ash") || weatherType.equals("Squall")
-                    || weatherType.equals("Tornado"))
+                    || weatherType.equals("Tornado")) {
                         weatherIcon.setImageResource(R.drawable.mist);
-                    else if(weatherType.equals("Clear"))
+                        homeLinearLayout.setBackgroundResource(R.drawable.cloudy_bg);
+                    }
+                    else if(weatherType.equals("Clear")) {
                         weatherIcon.setImageResource(R.drawable.sunny);
-                    else if(weatherType.equals("Clouds"))
+                        homeLinearLayout.setBackgroundResource(R.drawable.sunny_bg);
+                    }
+                    else if(weatherType.equals("Clouds")) {
                         weatherIcon.setImageResource(R.drawable.broken_clouds);
+                        homeLinearLayout.setBackgroundResource(R.drawable.cloudy_bg);
+                    }
+
 
 
                     t2_city.setText(cityName);
                     t3_weather_type.setText(weatherType);
 
-                    Calendar calendar = Calendar.getInstance();
-                    SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
-                    String formatted_date = sdf.format(calendar.getTime());
+                    description_textView.setText(desc);
 
-                    t4_date.setText(formatted_date);
+
+                    JSONObject wind_object = response.getJSONObject("wind");
+                    String wind_speed = wind_object.getString("speed");
+                    wind_speed_textView.setText(wind_speed + " km/h");
+
+                    String humidity = main_object.getString("humidity");
+                    humidity_textView.setText(humidity + "%");
+
+
+//                    Calendar calendar = Calendar.getInstance();
+//                    SimpleDateFormat sdf = new SimpleDateFormat("EEEE");
+//                    String formatted_date = sdf.format(calendar.getTime());
+//
+//                    t4_date.setText(formatted_date);
 
                     double temp_int = Double.parseDouble(temp) - 273.15;
                     int i = (int) Math.round(temp_int);
                     t1_temp.setText(String.valueOf(i));
 
-                    double temp_int_feels = Double.parseDouble(feelsLike) - 273.15;
-                    int j = (int) Math.round(temp_int_feels);
-                    t5_feels_like.setText(String.valueOf(j) + "°");
+
+                    String visibility = response.getString("visibility");
+                    double visb_int = Double.parseDouble(visibility) / 1000;
+                    visibility_textView.setText(String.valueOf(visb_int) + " km");
+
 
                 } catch (JSONException e) {
                     e.printStackTrace();
